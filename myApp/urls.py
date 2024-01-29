959c688dc25dcdbd110f221923d4d5e8668d425c
Josue <josueazc120@hotmail.com>
2024-01-28 23:32:48 -0500

    Proyecto Terminado

--- myApp/urls.py
+++ myApp/urls.py
@@ -5,10 +5,15 @@ urlpatterns = [
    path('', views.signin, name='home'),
    path('signin/', views.signin, name='signin'),
    path('signup/', views.signup),
+   path('signout/', views.signout, name="signout"),
    path('proveedor/', views.proveedor, name= "proveedor"),
    path('productos/', views.products, name="productos"),
    path('registrarProveedor/', views.registrarProveedor),
-   path('eliminarProveedor/<codigo>',views.eliminarProveedor),
+   path('registrarProducto/',views.registrarProducto),
+   path('eliminarProveedor/<codigo>', views.eliminarProveedor),
    path('edicionProveedor/<codigo>', views.edicionProveedor),
-   path('editarProveedor/', views.editarProveedor)
+   path('edicionProducto/<numero_de_serie>/', views.edicionProducto, name='edicionProducto'),
+   path('eliminarProducto/<numero_de_serie>/', views.eliminarProducto),
+   path('editarProducto/', views.editarProducto),
+   path('editarProveedor/', views.editarProveedor),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Proveedor, ComponenteElectronico
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def proveedor(request):
@@ -58,7 +58,9 @@ def signin(request):
     return render(request, 'signin.html')
 
 def signout(request):
-    return render(request, 'signout.html')
+    logout(request)
+    messages.success(request, "Ha cerrado sesión correctamente")
+    return redirect('home')
 
 def registrarProveedor(request):
     nombre = request.POST['nombre']
@@ -68,14 +70,48 @@ def registrarProveedor(request):
     proveedor = Proveedor.objects.create(nombre = nombre, direccion = direccion, telefono = telefono ,codigo_proveedor = codigo)
     return redirect('/proveedor')
 
+def registrarProducto(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        descripcion = request.POST['descripcion']
+        numero_de_serie = request.POST['numero_de_serie']
+        cantidad = request.POST['cantidad']
+        precio_unitario = request.POST['precio_unitario']
+        proveedor_id = request.POST['proveedor']
+
+        proveedor = Proveedor.objects.get(id=proveedor_id)
+
+        componente = ComponenteElectronico.objects.create(
+            nombre=nombre,
+            descripcion=descripcion,
+            numero_de_serie=numero_de_serie,
+            cantidad_disponible=cantidad,
+            precio_unitario=precio_unitario,
+            proveedor=proveedor
+        )
+        return redirect('/productos')
+
+
 def eliminarProveedor(request,codigo):
     proveedor = Proveedor.objects.get(codigo_proveedor = codigo)
     proveedor.delete()
     return redirect('/proveedor')
 
+def eliminarProducto(request, numero_de_serie):
+    try:
+        producto = ComponenteElectronico.objects.get(numero_de_serie=numero_de_serie)
+        producto.delete()
+        return redirect('productos')  
+    except ComponenteElectronico.DoesNotExist:
+        return render(request, 'products.html') 
+
 def edicionProveedor(request, codigo):
     proveedor = Proveedor.objects.get(codigo_proveedor = codigo)
     return render(request, 'edicionProveedor.html', {"proveedor": proveedor})
+
+def edicionProducto(request, numero_de_serie):
+    producto = ComponenteElectronico.objects.get(numero_de_serie=numero_de_serie)
+    return render(request, 'edicionProducto.html', {"producto": producto})
 
 def editarProveedor(request):
     nombre = request.POST['nombre']
@@ -91,3 +127,21 @@ def editarProveedor(request):
     
     return redirect('/proveedor')
 
+def editarProducto(request):
+    nombre = request.POST['nombre']
+    descripcion = request.POST['descripcion']
+    numero_de_serie = request.POST['numero_de_serie']
+    cantidad = request.POST['cantidad']
+    precio_unitario = request.POST['precio_unitario']
+    
+    componente = ComponenteElectronico.objects.get(numero_de_serie = numero_de_serie)
+
+    componente.nombre = nombre
+    componente.descripcion = descripcion
+    componente.cantidad_disponible = cantidad
+    componente.precio_unitario = precio_unitario
+    
+    componente.save()
+    
+    return redirect('/productos')
+    
